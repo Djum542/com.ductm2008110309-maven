9b38219b181a0195706bcad7b2d3c83d12c1a20f
Djum542 <djum54022@gmail.com>
2022-05-05 17:55:13 +0700

    maven

---
+++ src/test/java/com/ductm2008110309/AppTest.java
@@ -0,0 +1,20 @@
+package com.ductm2008110309;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
